d1034b7410f79c2d48acfd7642fa7009cb0c2abc
Renato Albuquerque <renatomalb83@gmail.com>
2023-10-14 01:39:19 -0300

    updated js function, result page, added input validations

--- js/scripts.js
+++ js/scripts.js
@@ -18,10 +18,17 @@ function cleanValues() {
 }
 
 function resultPage() {   
+    let inputHeight = document.querySelector(".inputHeight");
+    let inputWeight = document.querySelector(".inputWeight");
     let containerOne = document.querySelector(".container-1");
     let containerTwo = document.querySelector(".container-2");
-    containerOne.classList.add("hide");
-    containerTwo.classList.remove("hide");
+    
+    if((inputHeight.value == "") || (inputHeight.value == 0) || (inputWeight.value == "") || (inputWeight.value == 0)) {
+        alert("Please enter with a correct value. The values cannot be empty or zero.")
+    } else {
+        containerOne.classList.add("hide");
+        containerTwo.classList.remove("hide");
+    }
     
     // calcular imc
     // mostrar imc, text content
